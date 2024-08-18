4097712159c841374727180fec1bae61d9151dd4
anh vu <23110367@student.hcmute.edu.vn>
2024-08-18 10:43:33 +0700

    change 1

--- src/main/java/vn/hoidanit/laptopshop/hello.java
+++ src/main/java/vn/hoidanit/laptopshop/hello.java
@@ -5,9 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
  
 @RestController
 public class hello {
-         @GetMapping("/")
+     @GetMapping("/")
      public String index() {
-      return "Hello World from AnhVudev";
-            }
+         return "Hello World from AnhVudev love uuu";
+    }
+    @GetMapping("/user")
+     public String index_1() {
+         return "Hello World from user";
+    }
+    @GetMapping("/admin")
+     public String index_2() {
+         return "Hello World from admin ";
+    }
 
 }
